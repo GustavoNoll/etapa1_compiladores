8cfb77373f66631519a78e2817cca8dd25994243
Bruno Bastos <bruno.ufrgs2019@gmail.com>
2023-10-25 17:33:55 -0300

    Fix functions.c

--- functions.c
+++ functions.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 static int line_number = 1;     // Variável estática para rastrear o número da linha
-static int deep_comment = 0;    // Variável estática que armazena a profundidade dos comentários de bloco
 
 // Implementação da função para obter o número da linha
 int get_line_number(void) {
