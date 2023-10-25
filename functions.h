f64d78303f15cb8c4a9a8cfae5fd4270e90efb85
gustavo noll <gustavonoll78@gmail.com>
2023-10-25 09:39:42 -0300

    end

--- functions.h
+++ functions.h
@@ -1,5 +1,5 @@
 int get_line_number(void);
 void increment_line_number(void);
-int get_deep_cooment(void);
+int get_deep_comment(void);
 void increment_deep_comment(void);
 void decrement_deep_comment(void);
